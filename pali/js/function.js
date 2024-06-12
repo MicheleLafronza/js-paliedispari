b9167cf440fd8d9599d3394a36d3e94482e7d67f
MicheleLafronza <michele.lafronza91@gmail.com>
2024-06-12 15:59:12 +0200

    feat(pali): add some comments

--- pali/js/function.js
+++ pali/js/function.js
@@ -1,4 +1,6 @@
-// credo funzione per checkare la parola
+// creo funzione per checkare la parola
 function checkPalindrome(str) {
+
+    // questo linea di codice mi ritorna un valore booleano TRUE se la stringa dopo che è stata splittata, messa in reverse e ricostruita, rimane la stessa
     return str === str.split('').reverse().join('');
   }
