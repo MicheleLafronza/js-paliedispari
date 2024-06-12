58f1aa14f1afa3ca7ec95f79ba929ce05226afc8
MicheleLafronza <michele.lafronza91@gmail.com>
2024-06-12 16:26:55 +0200

    feat(pali): exercise pali completed

--- pali/js/function.js
+++ pali/js/function.js
@@ -1,6 +1,9 @@
 // creo funzione per checkare la parola
 function checkPalindrome(str) {
 
-    // questo linea di codice mi ritorna un valore booleano TRUE se la stringa dopo che è stata splittata, messa in reverse e ricostruita, rimane la stessa
-    return str === str.split('').reverse().join('');
+    // questa linea di codice mi ritorna un valore booleano TRUE assiociato alla variabile check, se la stringa dopo che è stata splittata, messa in reverse e ricostruita, rimane la stessa
+    let check = str === str.split('').reverse().join('');
+
+    // qui la funziona mi ritorna "true" se la parola è palindroma, "false" se non lo è
+    return check;
   }
--- pali/js/main.js
+++ pali/js/main.js
@@ -1,5 +1,23 @@
-// creo la variabile input e button
-let parola = document.getElementById("parola");
+// creo la variabile input, button e risposta
+let inputParola = document.getElementById("parola");
 const button = document.getElementById("btn");
+let risposta = document.getElementById("risultato");
+
+// creo evento per inviare la parola
+button.addEventListener ("click",
+
+    function () {
+        // creo variabile per riceve l'input
+        let parola = inputParola.value;
+        
+        // if,else con la funzione per stampare il risultato
+        if (checkPalindrome(parola) === true) {
+            risposta.innerHTML=("La parola è palindroma")
+        } else {
+            risposta.innerHTML=("La parola non è palindroma")
+        }
+        
+    }
 
 
+)
